1e1c09e754b0ae87801c83057ebedee090b36f42
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-07-15 15:40:05 +0300

    Fetch Convert button

--- script.js
+++ script.js
@@ -1,35 +1,49 @@
 const accessToken = "6jY5ZUmhFf4vw6ULUFhR4e9AJznJ01fO";
 const baseCurrency = document.getElementById("Base-currency")
 const targetCurrency = document.getElementById("Target-currency")
+const convertBtn = document.getElementById("convert-btn")
 
 document.addEventListener("DOMContentLoaded", (e) => {
-    
-    
-    fetch(`https://api.currencybeacon.com/v1/currencies?type=fiat&api_key=${accessToken}`)
-    
-    .then(response => response.json())
-    .then(currencies => {
-        console.log(currencies); 
-        populateCurrencies(currencies); 
 
-    })
-    .catch(error => {
-        console.error(error);
-        
-    });
+
+    fetch(`https://api.currencybeacon.com/v1/currencies?type=fiat&api_key=${accessToken}`)
+
+        .then(response => response.json())
+        .then(currencies => {
+            console.log(currencies);
+            populateCurrencies(currencies);
+
+        })
+        .catch(error => {
+            console.error(error);
+
+        });
 })
 
 
-function populateCurrencies (currencies) {
-currencies.forEach(currency => {
-    const baseOption = document.createElement("option")
-    baseOption.textContent = currency.short_code
-    baseOption.value = currency.short_code
-    baseCurrency.appendChild(baseOption);
+function populateCurrencies(currencies) {
+    currencies.forEach(currency => {
+        const baseOption = document.createElement("option")
+        baseOption.textContent = currency.short_code
+        baseOption.value = currency.short_code
+        baseCurrency.appendChild(baseOption);
 
-const targetOption = baseOption.cloneNode(true) 
-targetCurrency.appendChild(targetOption);
-    
-   
-});
+        const targetOption = baseOption.cloneNode(true)
+        targetCurrency.appendChild(targetOption);
+
+
+    });
 }
+
+convertBtn.addEventListener("click", (e) => {
+    console.log("ghjhgf")
+    e.preventDefault();
+
+    fetch(`https://api.currencybeacon.com/v1/convert?type=fiat&api_key=${accessToken}`)
+        .then(response => response.json())
+        .then(conversion => {
+            console.log(conversion);
+        })
+
+})
+
