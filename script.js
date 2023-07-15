1eac59e5807426f73e7a5435ea058e6523645812
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-07-15 15:16:23 +0300

    Populate Target Currencies

--- script.js
+++ script.js
@@ -2,43 +2,34 @@ const accessToken = "6jY5ZUmhFf4vw6ULUFhR4e9AJznJ01fO";
 const baseCurrency = document.getElementById("Base-currency")
 const targetCurrency = document.getElementById("Target-currency")
 
-
-baseCurrency.addEventListener("click", (e) => {
-    e.preventDefault();
+document.addEventListener("DOMContentLoaded", (e) => {
     
-   
-    fetch("https://api.currencybeacon.com/v1/currencies", {
-        method: "GET",
-        headers: {
-            Authorization: ` ${accessToken}`,
-        }
-    })
-        .then(response => response.json())
-        .then(baseCurrencies => {
-            console.log(baseCurrencies);  
+    
+    fetch(`https://api.currencybeacon.com/v1/currencies?type=fiat&api_key=${accessToken}`)
+    
+    .then(response => response.json())
+    .then(currencies => {
+        console.log(currencies); 
+        populateCurrencies(currencies); 
 
-        })
-        .catch(error => {
-            console.error(error);
-        });
+    })
+    .catch(error => {
+        console.error(error);
+        
+    });
 })
 
-targetCurrency.addEventListener("click", (e) => {
-    e.preventDefault();
+
+function populateCurrencies (currencies) {
+currencies.forEach(currency => {
+    const baseOption = document.createElement("option")
+    baseOption.textContent = currency.short_code
+    baseOption.value = currency.short_code
+    baseCurrency.appendChild(baseOption);
+
+const targetOption = baseOption.cloneNode(true) 
+targetCurrency.appendChild(targetOption);
     
    
-    fetch("https://api.currencybeacon.com/v1/currencies", {
-        method: "GET",
-        headers: {
-            Authorization: ` ${accessToken}`,
-        }
-    })
-        .then(response => response.json())
-        .then(targetCurrencies => {
-            console.log(targetCurrencies);  
-
-        })
-        .catch(error => {
-            console.error(error);
-        });
-})
+});
+}
